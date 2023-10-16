d76ca330281c4cfa4d04b0c8f34d85081a7832be
Hamid Zahid <hamidchaudhary3867@gmail.com>
2023-10-16 18:26:26 +0500

    toolbar typographt button etc

--- src/components/ContactUS.js
+++ src/components/ContactUS.js
@@ -7,14 +7,14 @@ const ContactUS = () => {
         <>
             <Box>
                 <Typography variant='h4' mt={6} mb={6} >Contact Us</Typography>
-                <TextField variant='outlined' label="First Name" mb="1" type='text' fullWidth />
-                <TextField variant='outlined' label="Last Name" mb="1" type='text' fullWidth />
-                <TextField variant='outlined' label="First Name" mb="1" type='text' fullWidth />
-                <TextField variant='outlined' label="My Name" mb="1" type='text' fullWidth />
-                <TextField variant='outlined' label="Your Name" mb="1" type='text' fullWidth />
-                <TextField variant='outlined' label="His Name" mb="1" type='text' fullWidth />
-                <TextField variant='outlined' label="Her Name" mb="1" multiline type='text' fullWidth />
-                <Button size='large' variant='contained' mt={3}> Button</Button>
+                <TextField variant='outlined' label="First Name" type='text' fullWidth />
+                <TextField variant='outlined' label="Last Name" type='text' fullWidth />
+                <TextField variant='outlined' label="First Name" type='text' fullWidth />
+                <TextField variant='outlined' label="My Name" type='text' fullWidth />
+                <TextField variant='outlined' label="Your Name" type='text' fullWidth />
+                <TextField variant='outlined' label="His Name" type='text' fullWidth />
+                <TextField variant='outlined' label="Her Name" multiline type='text' fullWidth />
+                <Button size='large' variant='contained' > Button</Button>
             </Box>
 
         </>
