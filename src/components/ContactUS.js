d4cb5e3be823a9f68a4eec0b7dfd614c97ac7caa
Hamid Zahid <hamidchaudhary3867@gmail.com>
2023-10-16 18:25:03 +0500

    toolbar typographt button etc

--- src/components/ContactUS.js
+++ src/components/ContactUS.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import { Typography, TextField } from '@mui/material'
+import { Typography, TextField, Box, Button } from '@mui/material'
+import { Formik } from 'formik'
 
 const ContactUS = () => {
     return (
         <>
-            <Typography variant='h4' mt={6} mb={6} >Contact Us</Typography>
-            <TextField variant='outlined' label="First Name" mb="1" type='text' fullWidth />
-            <TextField variant='outlined' label="Last Name" mb="1" type='text' fullWidth />
-            <TextField variant='outlined' label="First Name" mb="1" type='text' fullWidth />
-            <TextField variant='outlined' label="My Name" mb="1" type='text' fullWidth />
-            <TextField variant='outlined' label="Your Name" mb="1" type='text' fullWidth />
-            <TextField variant='outlined' label="His Name" mb="1" type='text' fullWidth />
-            <TextField variant='outlined' label="Her Name" mb="1" multiline type='text' fullWidth />
+            <Box>
+                <Typography variant='h4' mt={6} mb={6} >Contact Us</Typography>
+                <TextField variant='outlined' label="First Name" mb="1" type='text' fullWidth />
+                <TextField variant='outlined' label="Last Name" mb="1" type='text' fullWidth />
+                <TextField variant='outlined' label="First Name" mb="1" type='text' fullWidth />
+                <TextField variant='outlined' label="My Name" mb="1" type='text' fullWidth />
+                <TextField variant='outlined' label="Your Name" mb="1" type='text' fullWidth />
+                <TextField variant='outlined' label="His Name" mb="1" type='text' fullWidth />
+                <TextField variant='outlined' label="Her Name" mb="1" multiline type='text' fullWidth />
+                <Button size='large' variant='contained' mt={3}> Button</Button>
+            </Box>
+
         </>
     )
 }
