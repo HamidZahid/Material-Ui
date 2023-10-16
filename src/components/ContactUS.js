758eaccb85774f1db3bd8d459ce3c03d061c4286
Hamid Zahid <hamidchaudhary3867@gmail.com>
2023-10-16 18:26:38 +0500

    toolbar typographt button etc

--- src/components/ContactUS.js
+++ src/components/ContactUS.js
@@ -5,7 +5,7 @@ import { Formik } from 'formik'
 const ContactUS = () => {
     return (
         <>
-            <Box>
+            {/* <Box>
                 <Typography variant='h4' mt={6} mb={6} >Contact Us</Typography>
                 <TextField variant='outlined' label="First Name" type='text' fullWidth />
                 <TextField variant='outlined' label="Last Name" type='text' fullWidth />
@@ -15,7 +15,7 @@ const ContactUS = () => {
                 <TextField variant='outlined' label="His Name" type='text' fullWidth />
                 <TextField variant='outlined' label="Her Name" multiline type='text' fullWidth />
                 <Button size='large' variant='contained' > Button</Button>
-            </Box>
+            </Box> */}
 
         </>
     )
